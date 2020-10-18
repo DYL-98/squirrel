e28165559c64b6384538acd7b0a59615ead53e70
Daiyang Li <dl3393@instance-3.c.tfa-2020-fall-dl3393.internal>
2020-10-18 00:47:33 +0000

    Show detail page implemented.

--- squirrel/urls.py
+++ squirrel/urls.py
@@ -5,5 +5,6 @@ app_name = 'squirrel'
 
 urlpatterns = [
     path('map/', views.simple, name='map'),
-    path('sighting/', views.list_all, name='list_all'),
+    path('sightings/', views.list_all, name='list_all'),
+    path('details/<int:squirrel_id>/', views.show_detail, name='details'),
 ]
--- squirrel/views.py
+++ squirrel/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Squirrel
 from django.http import JsonResponse
-
+from django.shortcuts import get_object_or_404
 
 def simple(request):
     sightings = Squirrel.objects.values('latitude', 'longitude')[:100]
@@ -9,9 +9,14 @@ def simple(request):
     return render(request, 'squirrel/map_.html', context)
 
 def list_all(request):
-    all_sightings = Squirrel.objects.values('unique_squirrel_id', 'date')
+    all_sightings = Squirrel.objects.values('id', 'unique_squirrel_id', 'date')
     context = {'sightings': all_sightings}
     return render(request, 'squirrel/list_all.html', context)
+
+def show_detail(request, squirrel_id):
+    squirrel = get_object_or_404(Squirrel, pk=squirrel_id)
+    context = {'sighting': squirrel}
+    return render(request, 'squirrel/detail.html', context)
 
 
 
